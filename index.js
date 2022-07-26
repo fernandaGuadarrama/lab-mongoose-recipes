e6ed6cac895653101482bdeef77d9e5f3ba36b9d
Fernanda Guadarrama <fernanda10_star@hotmail.com>
2022-07-25 23:01:26 -0500

    done

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
+
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
@@ -17,7 +18,61 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+   Recipe.create ({
+          title: "Maruchan",
+          level: "Easy Peasy",
+          ingredients: [
+            "Maruchan",
+            "Water",
+            "Lemon",
+            "Salsa Valentina"
+          ],
+          cuisine: "Mexican",
+          dishType: "soup",
+          image: "https://superviquez.com/index.php?route=product/product&tag=SOPA%20INST%20MARUCHAN%20CAMARON/PIC&product_id=5377",
+          duration: 5,
+          creator: "Estudihambre",
+          created:""
+        }) 
+        .then(recipe =>
+          console.log(recipe))
+        .catch(err =>
+          console.log("ERROR!"))
+
+          Recipe.insertMany(data)
+          .then()
+          .catch(err =>
+            console.log("ERROR!"))
+          
+      //     Recipe.updateOne({title: "Maruchan"}, {duration:5})
+      //     .then(recipe => console.log("Update successful"))
+      //     .catch((err) => console.log(err))
+      //     })
+    
+      //     Recipe.find({}, {title: 1})
+      // .then(console.log)
+      // .catch((err) => console.log(err))
+
+      // Recipe.updateOne({title: "Rigatoni alla Genovese"}, {duration: 100})
+      // .then( recipe => console.log("create"))
+      // .catch((err) => console.log(err))
+
+
+      // Recipe.find({}, {title: 1, _id: 0})
+      // .then(console.log)
+      // .catch((err) => console.log(err))
+
+      // Recipe.deleteOne({title: "Carrot Cake"})
+      // .then( recipe => {
+      //   console.log("delete")
+      //   mongoose.connection.close(() => {
+      //     console.log('disconnect')
+      //     process.exit(0)
+      //     })
+      // })
+      // .catch((err) => console.log(err))
+  
+
+    .catch(error => {
+      console.error('Error connecting to the database', error);
+    });
